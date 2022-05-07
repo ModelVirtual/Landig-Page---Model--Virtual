49f6f846c478ebb536dafe4ecbc4d726b17bd72f
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-05-06 23:38:17 -0500

    Feat: Call to Action
    Added call to action feature.

---
+++ .idea/click_head.js
@@ -0,0 +1,13 @@
+var button = document.querySelector('.menu');
+var move_up = document.querySelectorAll('.move_up')
+var click_head = document.querySelector('.head-click');
+//console.log(move_up);
+
+button.addEventListener('click', () =>{
+    click_head.classList.toggle('aparecer')
+})
+move_up.forEach(move => {
+    move.addEventListener('click', () => {
+        click_head.classList.toggle('aparecer')
+    })
+})
